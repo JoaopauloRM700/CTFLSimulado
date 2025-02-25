ff5fa0335c6c918849df581f7c286cf714a85a32
JoaopauloRM700 <JoaopauloRM700@users.noreply.github.com>
2025-02-25 16:44:00 +0000

    deploy: f0b023ce2f820289f45e7ffc159d174eba43243a

--- assets/NotFound-CGjuT4wf.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BZI_CdwA.js";const t=()=>o.jsxs("div",{className:"not-found-container",children:[o.jsx("h1",{className:"not-found-title",children:"404 - Página não encontrada"}),o.jsx("p",{className:"not-found-text",children:"A página que você está procurando não existe ou foi movida."})]});export{t as default};
